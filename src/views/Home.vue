c5bbf9c593cf45949c9300893c4acb60667ba266
QiuYang <1435632265@qq.com>
2019-07-30 09:29:03 +0800

    layout fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -377,7 +377,7 @@ export default {
 
   .new-msg {
     overflow: hidden !important;
-    position: absolute;
+    position: fixed;
     left: 0; right: 0; top: 0; bottom: 0;
     overflow: auto;
     padding: 10px;
