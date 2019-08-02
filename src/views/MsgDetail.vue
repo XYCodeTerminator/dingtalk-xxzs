a445c2bac0ddde595a0537f468fda59cb6cf5fde
QiuYang <1435632265@qq.com>
2019-08-02 15:17:34 +0800

    fileList

--- src/views/MsgDetail.vue
+++ src/views/MsgDetail.vue
@@ -17,7 +17,7 @@
     </div>
     <div class="content">{{msgDetail.content}}</div>
     <div class="file-list">
-      <a-list
+      <!-- <a-list
         header="附件"
         size="small"
         itemLayout="horizontal"
@@ -25,11 +25,21 @@
       >
         <a-list-item slot="renderItem" slot-scope="file">
           <a-list-item-meta :description="bytesToSize(file.size)">
-            <a slot="title" :href="file.url">{{file.file_name}}</a>
+            <a slot="title" :href="file.url" :download="file.file_name">{{file.file_name}}</a>
             <a-icon slot="avatar" type="paper-clip" style="color: #1FAFFF;font-size: 22px;" />
           </a-list-item-meta>
         </a-list-item>
-      </a-list>
+      </a-list> -->
+      <div class="list-header">附&nbsp;&nbsp;&nbsp;件：</div>
+      <div class="list">
+        <div class="list-item" v-for="(file, index) in msgDetail.fileList" :key="index">
+          <a-icon type="paper-clip" style="color: #1FAFFF;font-size: 22px;" />
+          <div class="item-content">
+            <a class="content-title" :href="file.file_url" :download="file.file_name">{{file.file_name}}</a>
+            <div class="content-desc">{{bytesToSize(file.size)}}</div>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
@@ -105,10 +115,10 @@ export default {
     padding: 10px;
   }
   .title {
-    margin: 0 auto;
     padding: 20px;
     font-size: 16px;
     font-weight: 500;
+    text-align: center;
   }
   .info {
     border-bottom: solid #eee 1px;
@@ -132,6 +142,29 @@ export default {
     border-bottom: 1px solid #eee;
   }
   .file-list {
-    padding: 10px;
+    padding: 10px 0;
+    .list-header {
+      padding: 10px 0;
+      border-bottom: 1px solid #eee;
+    }
+    .list {
+      .list-item {
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        border-bottom: 1px solid #eee;
+        padding: 5px;
+      }
+      .item-content {
+        margin-left: 10px;
+        .content-title {
+          font-size: 15px;
+        }
+        .content-desc{
+          font-size: 13px;
+          color: rgba(0, 0, 0, 0.45);
+        }
+      }
+    }
   }
 </style>
